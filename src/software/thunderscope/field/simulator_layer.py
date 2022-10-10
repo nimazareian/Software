96f087cee3cfb5cdf68a9108046c7bc208927ebe
nima <nimazareian80@gmail.com>
2022-10-09 18:29:11 -0700

    fix createCircle bug, ai vs ai doesnt launch with bots

--- src/software/thunderscope/field/simulator_layer.py
+++ src/software/thunderscope/field/simulator_layer.py
@@ -47,8 +47,7 @@ class SimulatorLayer(FieldLayer):
         # Draw the ball from the simulator state
         painter.drawEllipse(
             self.createCircle(
-                sim_world_state.ball.p_y * MILLIMETERS_PER_METER,
-                -sim_world_state.ball.p_x * MILLIMETERS_PER_METER,
-                BALL_MAX_RADIUS_MILLIMETERS,
+                Point(x_meters=sim_world_state.ball.p_y, y_meters=-sim_world_state.ball.p_x),
+                BALL_MAX_RADIUS_METERS,
             )
         )
