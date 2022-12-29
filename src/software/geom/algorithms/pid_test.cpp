5385a573542101048c7c6809d7d5cbbd922ff933
nima <nimazareian80@gmail.com>
2022-12-29 10:57:25 -0800

    Update primitive executor to use PID for turning

--- src/software/geom/algorithms/pid_test.cpp
+++ src/software/geom/algorithms/pid_test.cpp
@@ -5,10 +5,17 @@
 
 TEST(PIDTest, test_constructor)
 {
-    PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+    double time_step = 0.025;
+    double max_vel = 4.0;
+    double max = max_vel * time_step;
+    double min = -max_vel * time_step;
+    double Kp = 0.1;
+    double Kd = 0.01;
+    double Ki = 0.0;
+    PID pid = PID(time_step, max, min, Kp, Kd, Ki);
 
     double val = 20;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 500; i++) {
         double inc = pid.calculate(0, val);
         LOG(INFO) << "val: " << val << " + inc: " << inc;
         val += inc;
