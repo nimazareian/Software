5b2f8b47604f0aae9310e634a13592c4bdb8db9c
nima <nimazareian80@gmail.com>
2023-11-17 13:53:20 -0800

    Comment out angular compensation

--- src/software/jetson_nano/primitive_executor.cpp
+++ src/software/jetson_nano/primitive_executor.cpp
@@ -130,7 +130,7 @@ std::unique_ptr<TbotsProto::DirectControlPrimitive> PrimitiveExecutor::stepPrimi
             Vector target_velocity                  = getTargetLinearVelocity();
             AngularVelocity target_angular_velocity = getTargetAngularVelocity();
 
-            target_velocity = target_velocity.rotate(-0.5 * target_angular_velocity);
+//            target_velocity = target_velocity.rotate(-0.5 * target_angular_velocity);
 
             auto output = createDirectControlPrimitive(
                 target_velocity, target_angular_velocity,
