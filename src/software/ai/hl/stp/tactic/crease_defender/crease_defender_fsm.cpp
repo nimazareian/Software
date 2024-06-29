c6d7730733f30838c13bc94cf1a58c20bebf16c5
Nima Zareian <nimazareian80@gmail.com>
2024-06-30 01:07:42 +0200

    Address comments

--- src/software/ai/hl/stp/tactic/crease_defender/crease_defender_fsm.cpp
+++ src/software/ai/hl/stp/tactic/crease_defender/crease_defender_fsm.cpp
@@ -44,8 +44,8 @@ void CreaseDefenderFSM::blockThreat(
     }
     else
     {
-        LOG(WARNING)
-            << "Could not find a point on the defense area to block a potential shot";
+//        LOG(WARNING)
+//            << "Could not find a point on the defense area to block a potential shot";
     }
     Angle face_threat_orientation =
         (event.control_params.enemy_threat_origin - event.common.robot.position())
